0d9873d774f194a68f41e7e92c3597df9e7c9718
Gentiana-svb <rowosmar31415@icloud.com>
2023-01-05 18:20:59 +0900

    setup tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+  import '../app.css'
+</script>
+
+<h1 class="text-2xl font-bold">Welcome to SvelteKit</h1>
 <p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  Visit
+  <a href="https://kit.svelte.dev" class="text-blue-500 underline">
+    kit.svelte.dev
+  </a>
+  to read the documentation
 </p>
